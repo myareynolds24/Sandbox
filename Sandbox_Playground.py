9fa3115b1135eaa348fd9a5d684ea663378f161b
Mya <02mya.reynolds@gulllakecs.org>
2023-10-12 08:27:04 -0400

    Add Print Lines Introducing Myself

--- Sandbox_Playground.py
+++ Sandbox_Playground.py
@@ -1,3 +1,7 @@
 # Developer: Mya Reynolds
 # Date 10.11.2023
 # Program: Sandbox Playground
+
+# 10.12.2023 practice
+print("Hello, Mya")
+print("\nI am looking forward to not being in this class :)")
